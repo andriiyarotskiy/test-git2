2d5611ee09707959097770c8a5ccf5ef12ad7f46
Andriy <refwol131@mail.ru>
2020-09-15 02:47:21 +0300

    Updates

---
+++ precache-manifest.b5644013545850d4e164b299cffca44b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4662b225d971c168c7c88f931603fa3",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "205dadf2ced3076d6c5e",
+    "url": "/counter/static/css/main.3d7b5ced.chunk.css"
+  },
+  {
+    "revision": "ccd95b0e20ee7a49a3f8",
+    "url": "/counter/static/js/2.813c1a22.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/counter/static/js/2.813c1a22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "205dadf2ced3076d6c5e",
+    "url": "/counter/static/js/main.67703157.chunk.js"
+  },
+  {
+    "revision": "ad0255c48e73deef29df",
+    "url": "/counter/static/js/runtime-main.5d0ccf1a.js"
+  }
+]);
